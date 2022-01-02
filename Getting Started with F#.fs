ce09bf568b696f18e081bbe34c15544d8921f96c
Michael Royal <45159366+mikeroyal@users.noreply.github.com>
2022-01-02 15:42:30 -0800

    Updated file.
    Added simple hello program.

--- Getting Started with F#.fs
+++ Getting Started with F#.fs
@@ -1,1 +1,9 @@
 Code samples & snippets coming soon!
+
+
+// Define a new function to print a name.
+let printGreeting name =
+    printfn $"Hello {name}!"
+
+// Call your new function!
+printGreeting "Mike"
